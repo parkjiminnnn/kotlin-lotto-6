69eeafa477a75f6a0febe7db3c6b898e2196cc4a
박지민 <pjm5489@naver.com>
2024-09-27 14:25:47 +0900

    feat: 상수 추가

--- src/main/kotlin/lotto/constants/Constant.kt
+++ src/main/kotlin/lotto/constants/Constant.kt
@@ -6,6 +6,8 @@ object Constant {
     const val INPUT_WINNING_NUMBERS_MSG = "당첨 번호를 입력해 주세요."
     const val INPUT_BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요."
     const val WIN_TOTAL_MSG = "당첨 통계"
+    const val LOTTO_NUMBER_SIZE = 6
+    const val LOTTO_PRICE = 1000
 
 
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,12 +1,13 @@
 package lotto.view
 
+import lotto.constants.Constant.LOTTO_PRICE
 import lotto.constants.Constant.PURCHASE_QUANTITY_MSG
 import lotto.constants.Constant.WIN_TOTAL_MSG
 
 class OutputView {
 
     fun purchaseQuantityMsg(money: Int) {
-        val buyCount = money/1000
+        val buyCount = money / LOTTO_PRICE
         println("$buyCount$PURCHASE_QUANTITY_MSG")
     }
 
