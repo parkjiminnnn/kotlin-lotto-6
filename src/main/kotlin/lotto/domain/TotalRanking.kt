8e3dcc11614c4fad57ea7196046e9609066014e5
박지민 <pjm5489@naver.com>
2024-09-29 04:19:04 +0900

    feat: TotalRanking생성

--- src/main/kotlin/lotto/domain/TotalRanking.kt
+++ src/main/kotlin/lotto/domain/TotalRanking.kt
@@ -1,7 +1,20 @@
 package lotto.domain
 
-enum class TotalRanking {
+enum class TotalRanking(count: Int,bous: Boolean,prizeMoney: Int) {
+    equalThree(3,false,5000),
+    equalFour(4,false,50000),
+    equalFive(5,false,15000000),
+    equalBonus(5,true,30000000),
+    equalSix(6,false,2000000000);
 
-
+//    fun printRanking(value: TotalRanking) {
+//        when(value) {
+//            equalThree -> TODO()
+//            equalFour -> TODO()
+//            equalFive -> TODO()
+//            equalBonus -> TODO()
+//            equalSix -> TODO()
+//        }
+//    }
 }
 
