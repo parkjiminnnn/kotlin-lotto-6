0a449247e9bb4e57d47c5b05f023d6124a826b1a
박지민 <pjm5489@naver.com>
2024-09-29 04:14:47 +0900

    feat: resultLotto메서드 생성

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -10,16 +10,25 @@ class LottoController {
     private val outputView = OutputView()
     private val makeList = MakeList()
     fun runLotto() {
-        InputView().inputMoneyMsg()
+        inputView.inputMoneyMsg()
         val money = readln().toInt()
+
         outputView.purchaseQuantityMsg(money)
         val lottoList = makeList.makeLottoList(money)
         outputView.outputLottoList(lottoList)
+
         inputView.inputWinNumbersMsg()
         val inputWinNumbers = readln()
-        val lotto = Lotto(makeList.makeNumberList(inputWinNumbers))
+
         inputView.inputBonusNumberMsg()
-        val inputBonusNumber = readln()
-        outputView.winTotalMsg()
+        val inputBonusNumber = readln().toInt()
+        resultLotto(inputWinNumbers, lottoList, inputBonusNumber)
+    }
+
+    private fun resultLotto(inputWinNumbers: String, lottoList: List<List<Int>>, inputBonusNumber: Int) {
+        outputView.winTotalMsg()//resultLotto에 파라미터 추가해서 구현후에 런로또에 넣기
+        val lotto = Lotto(makeList.makeNumberList(inputWinNumbers))
+        lotto.matchingNumbers(lottoList, inputBonusNumber)
+
     }
 }
