e1858b1f77479d5b6bf36942589737cbca80733c
박지민 <pjm5489@naver.com>
2024-09-29 04:14:25 +0900

    refactor: matchingNumbers기능 수정

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -8,14 +8,17 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    fun matchingNumbers(lottoList: List<List<Int>>): Int {
+    fun matchingNumbers(lottoList: List<List<Int>>, bonusNumber: Int): Pair<Int, Boolean> {
         var matchingCount = 0
+        var isBonusNumber = false
         for (i in lottoList.indices) {
-            for (j in numbers.indices)
-                if (lottoList[i].contains(numbers[j])) {
-                    matchingCount++
+            for (j in numbers.indices) {
+                when {
+                    lottoList[i].contains(numbers[j]) -> matchingCount++
                 }
+            }
+            if (lottoList[i].contains(bonusNumber)) isBonusNumber = true
         }
-        return matchingCount
+        return Pair(matchingCount, isBonusNumber)
     }
 }
